590ca05aa658b5f2f8b1d22dc494a2ef305ae578
newbe01 <pkwaa3@gmail.com>
2024-02-21 18:05:31 +0900

    test: userService, scheduleService test

---
+++ src/test/java/com/sparta/schedule/service/ScheduleServiceTest.java
@@ -0,0 +1,133 @@
+package com.sparta.schedule.service;
+
+import com.sparta.schedule.domain.Schedule;
+import com.sparta.schedule.domain.User;
+import com.sparta.schedule.dto.schedule.ScheduleRequestDto;
+import com.sparta.schedule.dto.schedule.ScheduleUpdateDto;
+import com.sparta.schedule.repository.ScheduleRepository;
+import com.sparta.schedule.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ScheduleServiceTest {
+
+    @Mock ScheduleRepository scheduleRepository;
+    @Mock UserRepository userRepository;
+    @InjectMocks ScheduleService scheduleService;
+
+    @DisplayName("할일 생성 테스트")
+    @Test
+    void createSchedule() {
+        // given
+        User user = new User("testUser", "testPw");
+        ScheduleRequestDto request = new ScheduleRequestDto("title", "content");
+        given(userRepository.findById(any())).willReturn(Optional.of(new User("testUser", "testPw")));
+        given(scheduleRepository.save(any(Schedule.class))).willReturn(new Schedule(request, user));
+
+        // when
+        Schedule schedule = scheduleService.createSchedule(request, user);
+
+        // then
+        then(userRepository).should().findById(user.getId());
+        then(scheduleRepository).should().save(any(Schedule.class));
+        assertThat(schedule.getUser()).isEqualTo(user);
+        assertThat(schedule.getTitle()).isEqualTo(request.getTitle());
+        assertThat(schedule.getContents()).isEqualTo(request.getContents());
+    }
+
+
+    @DisplayName("할일 단건조회 테스트")
+    @Test
+    void findOneTest() {
+        // given
+        Long id =1L;
+        User user = new User("testUser", "testPw");
+        ScheduleRequestDto request = new ScheduleRequestDto("title", "content");
+
+        given(scheduleRepository.findById(anyLong())).willReturn(Optional.of(new Schedule(request, user)));
+
+        // when
+        Schedule schedule = scheduleService.getSchedule(id);
+
+        // then
+        then(scheduleRepository).should().findById(anyLong());
+        assertThat(schedule.getUser()).isEqualTo(user);
+        assertThat(schedule.getTitle()).isEqualTo(request.getTitle());
+        assertThat(schedule.getContents()).isEqualTo(request.getContents());
+    }
+
+    @DisplayName("할일 리스트조회 테스트")
+    @Test
+    void findAllTest() {
+        // given
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+        given(userRepository.findAll()).willReturn(List.of(user1, user2, user3));
+
+        // when
+        List<User> schedules = scheduleService.getSchedules();
+
+        // then
+        then(userRepository).should().findAll();
+        assertThat(schedules.size()).isEqualTo(3);
+    }
+
+    @DisplayName("할일 수정 테스트")
+    @Test
+    void updateTest() {
+        // given
+        User user = new User("testUser", "testPw");
+        ScheduleRequestDto request = new ScheduleRequestDto("title", "content");
+
+        Schedule schedule = new Schedule(request, user);
+
+        ScheduleUpdateDto updateRequest = new ScheduleUpdateDto("update", "update");
+
+        given(scheduleRepository.findById(anyLong())).willReturn(Optional.of(schedule));
+
+        // when
+        Schedule updateSchedule = scheduleService.updateSchedule(1L, updateRequest, user);
+
+        // then
+        then(scheduleRepository).should().findById(anyLong());
+        assertThat(updateSchedule.getTitle()).isEqualTo(updateRequest.getTitle());
+        assertThat(updateSchedule.getContents()).isEqualTo(updateRequest.getContents());
+    }
+
+    @DisplayName("할일 완료 테스트")
+    @Test
+    void completionTest() {
+        // given
+        User user = new User("testUser", "testPw");
+        ScheduleRequestDto request = new ScheduleRequestDto("title", "content");
+        Schedule schedule = new Schedule(request, user);
+
+        given(scheduleRepository.findById(anyLong())).willReturn(Optional.of(schedule));
+
+        // when
+        scheduleService.completeSchedule(1L, user);
+
+        // then
+        then(scheduleRepository).should().findById(anyLong());
+        assertThat(schedule.isCompletionYn()).isTrue();
+    }
+
+}
---
+++ src/test/java/com/sparta/schedule/service/UserServiceTest.java
@@ -0,0 +1,56 @@
+package com.sparta.schedule.service;
+
+import com.sparta.schedule.domain.User;
+import com.sparta.schedule.dto.user.UserSignRequest;
+import com.sparta.schedule.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock UserRepository userRepository;
+    @InjectMocks
+    BCryptPasswordEncoder passwordEncoder;
+
+    @DisplayName("회원가입 테스트")
+    @Test
+    void userSignupTest() {
+        // given
+        UserSignRequest request = new UserSignRequest("testName", "testPw");
+        UserService userService = new UserService(userRepository, passwordEncoder);
+
+        // when
+        userService.userSignup(request);
+        given(userRepository.findByUsername("testName")).willReturn(Optional.of(new User("testName", passwordEncoder.encode("testPw"))));
+
+        // then
+        User savedUser = userRepository.findByUsername("testName").orElseThrow();
+        assertThat(savedUser.getUsername()).isEqualTo("testName");
+        assertThat(passwordEncoder.matches("testPw", savedUser.getPassword())).isTrue();
+    }
+
+    @DisplayName("회원가입 테스트 실패")
+    @Test
+    void userSignupTest2() {
+        // given
+        UserSignRequest request = new UserSignRequest("testName", "testPw");
+        UserService userService = new UserService(userRepository, passwordEncoder);
+
+        // when & then
+        given(userRepository.findByUsername("testName")).willReturn(Optional.of(new User("testName", passwordEncoder.encode("testPw"))));
+        assertThatThrownBy(() -> userService.userSignup(request)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
